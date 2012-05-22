562e35f4d2c6becbdb7bf21e0f5224ecbe1b9576
willnorris <willnorris@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-22 01:36:00 +0000

    tag v1.3
    git-svn-id: https://plugins.svn.wordpress.org/opengraph/tags/1.3@547249 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- opengraph.php
+++ opengraph.php
@@ -5,7 +5,7 @@
  Description: Adds Open Graph metadata to your pages
  Author: Will Norris
  Author URI: http://willnorris.com/
- Version: 1.2
+ Version: 1.3
  License: Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0.html)
  Text Domain: opengraph
  */
@@ -17,15 +17,25 @@ $opengraph_prefix_set = false;
 
 /**
  * Add Open Graph XML prefix to <html> element.
+ *
+ * @uses apply_filters calls 'opengraph_prefixes' filter on RDFa prefix array
  */
 function opengraph_add_prefix( $output ) {
-  global $opengraph_prefix_set;
-  $opengraph_prefix_set = true;
-  
+  $prefixes = array(
+    'og' => OPENGRAPH_PREFIX_URI
+  );
+  $prefixes = apply_filters('opengraph_prefixes', $prefixes);
+
+  $prefix_str = '';
+  foreach( $prefixes as $k => $v ) {
+    $prefix_str .= $k . ': ' . $v . ' ';
+  }
+  $prefix_str = trim($prefix_str);
+
   if (preg_match('/(prefix\s*=\s*[\"|\'])/i', $output)) {
-    $output = preg_replace('/(prefix\s*=\s*[\"|\'])/i', '${1}og: ' . esc_attr(OPENGRAPH_PREFIX_URI) . ' ', $output);
+    $output = preg_replace('/(prefix\s*=\s*[\"|\'])/i', '${1}' . $prefix_str, $output);
   } else {
-    $output .= ' prefix="og: ' . esc_attr(OPENGRAPH_PREFIX_URI) . '"';
+    $output .= ' prefix="' . $prefix_str . '"';
   }
   return $output;
 }
@@ -41,27 +51,19 @@ add_filter('language_attributes', 'opengraph_add_prefix');
 function opengraph_metadata() {
   $metadata = array();
 
-   // defualt properties defined at http://opengraphprotocol.org/
+   // defualt properties defined at http://ogp.me/
   $properties = array(
     // required
     'title', 'type', 'image', 'url',
 
     // optional
-    'site_name', 'description',
-
-    // location
-    'longitude', 'latitude', 'street-address', 'locality', 'region',
-    'postal-code', 'country-name',
-
-    // contact
-    'email', 'phone_number', 'fax_number',
+    'audio', 'description', 'determiner', 'locale', 'site_name', 'video',
   );
 
   foreach ($properties as $property) {
     $filter = 'opengraph_' . $property;
     $metadata["og:$property"] = apply_filters($filter, '');
   }
-
   return apply_filters('opengraph_metadata', $metadata);
 }
 
@@ -75,8 +77,9 @@ function opengraph_default_metadata() {
   add_filter('opengraph_image', 'opengraph_default_image', 5);
   add_filter('opengraph_url', 'opengraph_default_url', 5);
 
-  add_filter('opengraph_site_name', 'opengraph_default_sitename', 5);
   add_filter('opengraph_description', 'opengraph_default_description', 5);
+  add_filter('opengraph_locale', 'opengraph_default_locale', 5);
+  add_filter('opengraph_site_name', 'opengraph_default_sitename', 5);
 }
 add_filter('wp', 'opengraph_default_metadata');
 
@@ -89,7 +92,6 @@ function opengraph_default_title( $title ) {
     global $post;
     $title = $post->post_title;
   }
-
   return $title;
 }
 
@@ -98,7 +100,11 @@ function opengraph_default_title( $title ) {
  * Default type property.
  */
 function opengraph_default_type( $type ) {
-  if ( empty($type) ) $type = 'blog';
+  if ( is_singular( array('post', 'page', 'aside', 'status') ) && empty($type) ) {
+    $type = 'article';
+  } elseif ( empty($type) ) {
+    $type = 'blog';
+  }
   return $type;
 }
 
@@ -116,7 +122,6 @@ function opengraph_default_image( $image ) {
       }
     }
   }
-
   return $image;
 }
 
@@ -125,7 +130,7 @@ function opengraph_default_image( $image ) {
  * Default url property, using the permalink for the page.
  */
 function opengraph_default_url( $url ) {
-  if ( empty($url) ) $url = get_permalink();
+  if ( is_singular() && empty($url) ) $url = get_permalink();
   return $url;
 }
 
@@ -143,8 +148,26 @@ function opengraph_default_sitename( $name ) {
  * Default description property, using the bloginfo description.
  */
 function opengraph_default_description( $description ) {
-  if ( empty($description) ) $description = get_bloginfo('description');
+  if ( is_singular() && empty($description) ) {
+    if ( has_excerpt() ) {
+      $description = get_the_excerpt();
+    } else {
+      global $post;
+      $description = wp_trim_words( strip_shortcodes($post->post_content), 25, '...' );
+    }
+  } elseif ( empty($description) ) {
+    $description = get_bloginfo('description');
+  }
   return $description;
+}
+
+
+/**
+ * Default locale property, using the WordPress locale.
+ */
+function opengraph_default_locale( $locale ) {
+  if ( empty($locale) ) $locale = get_locale();
+  return $locale;
 }
 
 
@@ -152,17 +175,16 @@ function opengraph_default_description( $description ) {
  * Output Open Graph <meta> tags in the page header.
  */
 function opengraph_meta_tags() {
-  global $opengraph_prefix_set;
-
-  $prefix = '';
-  if ( !$opengraph_prefix_set ) {
-    $prefix = 'prefix="og: ' . esc_attr(OPENGRAPH_PREFIX_URI) . '" ';
-  }
-
   $metadata = opengraph_metadata();
   foreach ( $metadata as $key => $value ) {
     if ( empty($key) || empty($value) ) continue;
-    echo '<meta ' . $prefix . 'property="' . esc_attr($key) . '" content="' . esc_attr($value) . '" />' . "\n";
+    if ( is_array( $value ) ) {
+      foreach ( $value as $v ) {
+        echo '<meta property="' . esc_attr($key) . '" content="' . esc_attr($v) . '" />' . "\n";
+      }
+    } else {
+      echo '<meta property="' . esc_attr($key) . '" content="' . esc_attr($value) . '" />' . "\n";
+    }
   }
 }
 add_action('wp_head', 'opengraph_meta_tags');
--- readme.txt
+++ readme.txt
@@ -1,8 +1,12 @@
 === Open Graph ===
 Contributors: willnorris
-Tags: social, opengraph, facebook
-Tested up to: 3.3.1
-Stable tag: 1.2
+Tags: social, opengraph, ogp, facebook
+Requires at least: 2.3
+Tested up to: 3.3.2
+Stable tag: 1.3
+License: Apache License, Version 2.0
+License URI: http://www.apache.org/licenses/LICENSE-2.0.html
+
 
 Add Open Graph metadata to your pages.
 
@@ -19,7 +23,7 @@ Note that this plugin does not actually add the Facebook Like Button to your
 pages, or do anything directly with the Open Graph data.  It makes the data
 available so that other services can do interesting things with it.
 
-[Open Graph Protocol]: http://opengraphprotocol.org/
+[Open Graph Protocol]: http://ogp.me/
 [Facebook Like Button]: http://developers.facebook.com/docs/reference/plugins/like
 
 
@@ -34,31 +38,42 @@ Graph property name.  For example, if you have a plugin that defines a custom
 post type named "movie", you could override the Open Graph 'type' property for
 those posts using a function like:
 
-  function my_og_type( $type ) {
-      if ( get_post_type() == "movie" ) {
-          $type = "movie";
-      }
-      return $type;
-  }
-  add_filter( 'opengraph_type', 'my_og_type' );
+    function my_og_type( $type ) {
+        if ( get_post_type() == "movie" ) {
+            $type = "movie";
+        }
+        return $type;
+    }
+    add_filter( 'opengraph_type', 'my_og_type' );
 
 This will work for all of the core Open Graph properties.  However, if you want
 to add a custom property, such as 'fb:admin', then you would need to hook into
 the `opengraph_metadata` filter.  This filter is passed an associative array,
 whose keys are the qualified Open Graph property names.  For example:
 
-  function my_og_metadata( $metadata ) {
-      $metadata['fb:admin'] = '12345,67890';
-      return $metadata;
-  }
-  add_filter( 'opengraph_metadata', 'my_og_metadata' );
+    function my_og_metadata( $metadata ) {
+        $metadata['fb:admin'] = '12345,67890';
+        return $metadata;
+    }
+    add_filter( 'opengraph_metadata', 'my_og_metadata' );
 
-Note that it is your responsibility to make sure that the XML namespace is
-registered for the prefix that you use.  To see one method for doing this, see
-the `opengraph_add_namespace` function in the Open Graph plugin.
+Note that you may need to define the RDFa prefix for your properties.  Do this
+using the `opengraph_prefixes` filter.
 
 
 == Changelog ==
+
+= version 1.3 (May 21, 2012) =
+ - add 'opengraph_prefixes' filter for defining additional prefixes
+ - add new basic properties, and remove some old ones.  This is a breaking
+   change for anyone that was using the old properties, but they can always be
+   added using the 'opengraph_metadata' filter.  (see [f476552][] for details)
+ - updates to many default values, particularly for individual posts and pages
+   (thanks pfefferle)
+ - add basic support for array values (see [d987eb7][])
+
+[f476552]: https://github.com/willnorris/wordpress-opengraph/commit/f47655202d59c0e5b5032b4b86764f7a87813640
+[d987eb7]: https://github.com/willnorris/wordpress-opengraph/commit/d987eb76e2da1431e5df3311fde3d9c2407b06f5
 
 = version 1.2 (Feb 21, 2012) =
  - switch to newer RDFa prefix syntax rather than XML namespaces (props
@@ -69,3 +84,4 @@ the `opengraph_add_namespace` function in the Open Graph plugin.
 
 = version 1.0 (Apr 24, 2010) =
  - initial public release
+
